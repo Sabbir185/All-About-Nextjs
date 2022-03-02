5638bfdc8d5594f79bb027005d53a8d260e9fc1c
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2022-03-02 13:29:42 +0600

    next routing and 404 page done

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,14 +1,29 @@
 import { useRouter } from "next/router";
 
-const ClientProjectPage = () => {
-  const router = useRouter();
-  console.log(router.query);
+function ClientProjectPage() {
+	const router = useRouter();
+	console.log(router.query);
 
-  return (
-    <div>
-      <h1>The project of a given client</h1>
-    </div>
-  );
-};
+	// function loadProjectHandler() {
+	// 	// data loading...
+	// 	router.push("/clients/sabbir/projectA");
+	// }
+
+  // alternative
+	function loadProjectHandler() {
+		// data loading...
+		router.push({
+			pathname: "/clients/[id]/[clientprojectid]",
+			query: { id: "motiur", clientprojectid: "project1" },
+		});
+	}
+
+	return (
+		<div>
+			<h1>The project of a given client</h1>
+			<button onClick={loadProjectHandler}>Load Project A</button>
+		</div>
+	);
+}
 
 export default ClientProjectPage;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
+import Link from "next/link";
+
 function HomePage() {
-    return (
-        <div>
-            <h1 className="bg-red-500 text-center">Welcome bro</h1>
-        </div>
-    );
-};
+	return (
+		<div>
+			<h1 className="bg-red-500 text-center">Welcome Nextjs World!</h1>
+
+			<ul>
+				<li>
+					<Link href="/portfolio">Portfolio</Link>
+				</li>
+				<li>
+					<Link href="/clients">Clients</Link>
+				</li>
+			</ul>
+		</div>
+	);
+}
 
 export default HomePage;
