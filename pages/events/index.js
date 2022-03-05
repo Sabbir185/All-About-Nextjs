73942f7500caaae166eccc92cb0d000b3d25fad1
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2022-03-05 13:49:00 +0600

    all event showed

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,14 @@
-import { getFeaturedEvents } from "../../data/dummy-data";
+// import { getFeaturedEvents } from "../../data/dummy-data";
+import { getAllEvents } from "../../data/dummy-data";
 import EventList from "../../components/events/event-list";
 
 
 export default function AllEventsPage() {
-	const featuredEvents = getFeaturedEvents();
+	const events = getAllEvents();
 
 	return (
 		<div>
-			<EventList items={featuredEvents} />
+			<EventList items={events} />
 		</div>
 	);
 }
