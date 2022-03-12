305ad0256191efad262e2d22d83f0670f47f55ee
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2022-03-12 12:44:10 +0600

    getStaticProps: props, revalidate, redirect, notFound

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,49 @@
-import Link from "next/link";
+import fs from "fs/promises";
+import path from "path";
 
-function HomePage() {
+function HomePage(props) {
+	const { products } = props;
+
 	return (
 		<div>
-			<ul>
-				<li>Product 1</li>
-				<li>Product 2</li>
-				<li>Product 3</li>
-				<li>Product 4</li>
-			</ul>
+			{products?.map((product) => (
+				<li key={product.id}>{product.title}</li>
+			))}
 		</div>
 	);
+}
+
+export async function getStaticProps(context) {
+	const filePath = path.join(process.cwd(), "data", "dummy-data.json");
+	const jsonData = await fs.readFile(filePath);
+	const data = JSON.parse(jsonData);
+
+	// use case of redirect
+	if (!data) {
+		return {
+			redirect: {
+				destination: "/something-page",
+			},
+		};
+	}
+
+	// use case of notFound
+	if(data.products.length ===0 ) {
+		return {
+			notFound: true
+		}
+	}
+
+	return {
+		props: {
+			products: data.products,
+		},
+		revalidate: 10,
+		// auto re-render / load data every 10 seconds helps for production mode
+		// some other special properties
+		// redirect: '/something-page',
+		// notFound: true // or false
+	};
 }
 
 export default HomePage;
